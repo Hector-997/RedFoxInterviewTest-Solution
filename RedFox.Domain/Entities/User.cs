79b86cbaa5c97beb588027d1f74a390105eb100c
Fernando Coluchi <hcr.rchhector@gmail.com>
2025-07-19 14:33:18 -0300

    make address optional

--- RedFox.Domain/Entities/User.cs
+++ RedFox.Domain/Entities/User.cs
@@ -14,6 +14,6 @@ public class User
 
     public string Website { get; set; } = string.Empty;
     public int CompanyId { get; set; }
-    public Address Address { get; set; }
+    public Address? Address { get; set; }
     public Company? Company { get; set; }
 }
