4bf1d0067ad0beed198017f7afb0ae23b784074c
Fernando Coluchi <hcr.rchhector@gmail.com>
2025-07-19 14:33:50 -0300

    configure address in entity

--- RedFox.Infrastructure/Configuration/UserConfiguration.cs
+++ RedFox.Infrastructure/Configuration/UserConfiguration.cs
@@ -15,5 +15,51 @@ public class UserConfiguration : IEntityTypeConfiguration<User>
         builder.HasKey(u => u.Id);
         builder.Property(u => u.Name).IsRequired().HasMaxLength(100);
         builder.Property(u => u.Username).IsRequired().HasMaxLength(50);
+
+        public class UserConfiguration : IEntityTypeConfiguration<User>
+    {
+        public void Configure(EntityTypeBuilder<User> builder)
+        {
+            builder.HasKey(u => u.Id);
+
+            builder.Property(u => u.Name)
+                .IsRequired()
+                .HasMaxLength(100);
+
+            builder.Property(u => u.Username)
+                .IsRequired()
+                .HasMaxLength(50);
+
+            builder.OwnsOne(u => u.Address, address =>
+            {
+                address.Property(a => a.Street)
+                    .IsRequired()
+                    .HasMaxLength(200);
+
+                address.Property(a => a.Suite)
+                    .HasMaxLength(100);
+
+                address.Property(a => a.City)
+                    .IsRequired()
+                    .HasMaxLength(100);
+
+                address.Property(a => a.Zipcode)
+                    .IsRequired()
+                    .HasMaxLength(20);
+
+                address.OwnsOne(a => a.Geo, geo =>
+                {
+                    geo.Property(g => g.Latitude)
+                        .IsRequired()
+                        .HasMaxLength(50);
+
+                    geo.Property(g => g.Longitude)
+                        .IsRequired()
+                        .HasMaxLength(50);
+                });
+            });
+        }
     }
+
+}
 }
