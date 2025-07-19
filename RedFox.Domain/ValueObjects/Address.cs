e2b88b8703f05752ed55aabe8a930c0832e0b13a
Fernando Coluchi <hcr.rchhector@gmail.com>
2025-07-19 15:28:54 -0300

    configure address vo for ef core support

--- RedFox.Domain/ValueObjects/Address.cs
+++ RedFox.Domain/ValueObjects/Address.cs
@@ -1,8 +1,10 @@
 ﻿namespace RedFox.Domain.ValueObjects;
 
-public record Address(
-    string Street,
-    string Suite,
-    string City,
-    string Zipcode,
-    Geolocation Geo);
+public record Address
+{
+    public required string Street { get; init; }
+    public required string Suite { get; init; }
+    public required string City { get; init; }
+    public required string Zipcode { get; init; }
+    public required Geolocation Geo { get; init; }
+}
